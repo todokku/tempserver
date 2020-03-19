0adcb1b7db0c7fd4d32017294c2f7d31a86db344
mikehellman <62214988+mikehellman@users.noreply.github.com>
2020-03-19 14:47:09 +0200

    init

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const fetch = require("node-fetch");
+const port = 5000;
+// Connection to the mongo client
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const assert = require("assert");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const CONNECTION_URL =
+  "mongodb+srv://mike:hellman@dashboard-vcavt.azure.mongodb.net/test?retryWrites=true&w=majority";
+const DATABASE_NAME = "dashboard";
+let database;
+let collection;
+
+app.listen(process.env.PORT || port, () => {
+  MongoClient.connect(
+    CONNECTION_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (error, client) => {
+      if (error) {
+        throw error;
+      }
+      database = client.db(DATABASE_NAME);
+      collection = database.collection("masal");
+      console.log("Connected to `" + DATABASE_NAME + "`!");
+    }
+  );
+});
+
+app.get("/getAllTasks", async (req, res) => {
+  collection.find({}).toArray((err, result) => {
+    res.send(result);
+  });
+});
+
+app.post("/insertTask", async (req, res) => {
+  const doc = req.body;
+  collection.insertOne(doc, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      collection.find({}).toArray((err, result) => {
+        res.send(result);
+        console.log(result);
+      });
+    }
+  });
+});
+
+app.post("/deleteTask", async (req, res) => {
+  const id = req.body.id;
+  console.log(id);
+  collection.deleteOne({ _id: ObjectId(id) }).then(deleted => {
+    collection.find({}).toArray((err, result) => {
+      res.send(result);
+    });
+  });
+});
+
+app.post("/updateTask/:id", async (req, res) => {
+  const data = req.body;
+  collection.replaceOne({ _id: ObjectId(req.params.id) }, data).then(result => {
+    collection.find({}).toArray((err, result) => {
+      res.send(result);
+    });
+  });
+});
+
+/* TEST - ONLY UNCOMMENT IF AND ONLY IF THERE IS A CRITICAL DB/SERVER CONNECTION ERROR - UNCOMMENT ABOVE IN CLIENT CONNECT
+const insertDocumentTester = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection("masal");
+  // Insert some documents
+  collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+};*/
