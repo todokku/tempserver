e8b2d6053dcccc566e9a780648be5507b80a9a50
ben <bensilvan3@gmail.com>
2020-03-31 17:59:14 +0300

    add api route to get a SAS token for read, write, list permissions for a specific container

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const fetch = require('node-fetch');
 const Config = require('./config');
 const DAL = require('./dal');
 const bodyParser = require('body-parser');
+const azureStore = require('azure-storage');
 
+const AZURE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=masal;AccountKey=mtM0kYLYCnJBd5m0HiUsZs3R0W9RCFWNqPHfiYSm0TKFa5EHXnmv4a4KlzxyrGRT44i50Z8tBnPVjE/rrY7hew==;EndpointSuffix=core.windows.net"
 const PORT = 8080;
+var blobService;
+
+try {
+  blobService = azureStore.createBlobService(AZURE_CONNECTION_STRING)
+}
+catch (exception) {
+  console.log("could not access azure store, requests for token will be failed")
+}
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -18,7 +28,7 @@ app.listen(process.env.PORT || PORT, async () => {
   await DAL.init(Config.mongoUrl, Config.dbName);
 
   const Controllers = require('./controllers'); //must be after DB initialization
-  Controllers.forEach(({controller, baseRoute}) => {
+  Controllers.forEach(({ controller, baseRoute }) => {
     app.use(`/${baseRoute}`, controller)
   })
 });
@@ -32,3 +42,25 @@ app.get('/getCorona', async (req, res) => {
   const cResponse = await cData.json();
   res.send(cResponse);
 });
+
+app.get('/token/:container', (async (req, res, next) => {
+  try {
+    const container = req.params.container;
+    const permissions = azureStore.BlobUtilities.SharedAccessPermissions.READ +
+      azureStore.BlobUtilities.SharedAccessPermissions.WRITE +
+      azureStore.BlobUtilities.SharedAccessPermissions.LIST;
+
+    var sharedAccessPolicy = {
+      AccessPolicy: {
+        Permissions: permissions,
+      }
+    };
+
+    const token = blobService.generateSharedAccessSignature(container, null, sharedAccessPolicy)
+    res.send(token);
+  }
+  catch (err) {
+    console.log(exception);
+    next(err);
+  }
+}))
